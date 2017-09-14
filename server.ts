68a3aa14049755266559fb2eff038debeeddbdb5
sye23 <shalomeisenbach@gmail.com>
2017-09-14 12:57:15 -0400

    this should work

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 const app = express();
 import { router } from './routes';
+import * as path from 'path';
 import { authRouter } from './authRoutes';
 import { checkToken } from './tokenCheck';
 
@@ -11,6 +12,13 @@ app.use(bodyParser.json());
 
 app.use('/auth', authRouter);
 app.use('/api', checkToken, router);
+
+if (process.env.PROD) {
+    app.use(express.static(path.join(__dirname, './frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+    });
+}
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
