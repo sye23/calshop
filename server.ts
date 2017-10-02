d9c64d968922bce828b06615b11ea84ef5788cff
sye23 <shalomeisenbach@gmail.com>
2017-10-01 23:15:39 -0400

    removed http redirect to https

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 require('dotenv').config();
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-const sslRedirect = require('heroku-ssl-redirect');
 const app = express();
 import { router } from './routes';
 import * as path from 'path';
@@ -10,7 +9,6 @@ import { checkToken } from './tokenCheck';
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(sslRedirect(['production'], 301));
 
 app.use('/auth', authRouter);
 app.use('/api', checkToken, router);
